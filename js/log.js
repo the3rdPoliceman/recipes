78701c0f7e02e6ae4b612f4d6ed2382a041d090b
the3rdPoliceman <gabbymoore@gmail.com>
2020-10-26 15:53:38 +0100

    made cooking log entry details collapsable

--- js/log.js
+++ js/log.js
@@ -2,6 +2,7 @@ $(".log-entry").each(function(index, element){
 	$(element).find(".log-header").attr("data-toggle", "collapse");
 	$(element).find(".log-header").attr("role", "button");
 	$(element).find(".log-header").attr("href", "#collapseExample" + index);
+	$(element).find(".log-header").addClass("collapsed");
 
 	$(element).find(".log-detail").addClass("collapse");
 	$(element).find(".log-detail").attr("id", "collapseExample" + index);
