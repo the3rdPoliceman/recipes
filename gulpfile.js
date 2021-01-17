7738bd878b10895bbbf2b8e75d1acaf4e2a798f2
the3rdPoliceman <gabbymoore@gmail.com>
2021-01-17 18:57:16 +0100

    updated gulpfile to fully build local target directory and dist directory separately

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ const {
   watch
 } = require('gulp');
 
-var sass = require('gulp-sass'),
+const sass = require('gulp-sass'),
   browserSync = require('browser-sync'),
   del = require('del'),
   imagemin = require('gulp-imagemin'),
@@ -20,49 +20,131 @@ var sass = require('gulp-sass'),
   htmlmin = require('gulp-htmlmin'),
   doT = require('dot'),
   fs = require('fs'),
-  path = require("path"),
-  fs_extra = require('fs-extra');
+  fs_extra = require('fs-extra'),
+  path = require("path");
 
 
 function compileScss() {
-  src('./scss/*.scss')
+  return src('./scss/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(dest('./css'));
+    .pipe(dest('./target/css'));
 }
 
-function watchScss() {
-  watch('./css/*.scss', compileScss);
+function getAllJsonFiles(dir, files) {
+  fs.readdirSync(dir).forEach(file => {
+    const current_file = path.join(dir, file);
+    if (fs.statSync(current_file).isDirectory())
+      return getAllJsonFiles(current_file, files);
+    else if (current_file.endsWith('.json'))
+      return files.push(current_file);
+  });
+}
+
+function generateRecipesFromJson(callback) {
+  let template_as_string = fs.readFileSync('doT/recipe-template.jst', 'utf8');
+  let templateFunction = doT.template(template_as_string);
+
+  let files = [];
+  getAllJsonFiles("json/Recipes", files);
+  for (let index = 0; index < files.length; index++) {
+    let recipe_as_string = fs.readFileSync(files[index], 'utf8');
+    let recipe_as_json = JSON.parse(recipe_as_string);
+    let recipe_as_html = templateFunction(recipe_as_json);
+
+    let generated_recipe_file_path = "gen/" + files[index].substring(5).replace('.json', '.html');
+    fs_extra.ensureFileSync(generated_recipe_file_path);
+    fs.writeFileSync(generated_recipe_file_path, recipe_as_html);
+  }
+
+  callback();
+};
+
+function watchAll() {
+  let filesToWatch = ['./scss/*.scss',
+    './json/**/*.json',
+    './images/*.*',
+    './js/*.*',
+    './doT/**/*.jst',
+    './Guides/**/*.html',
+    './Resources/**/*.html',
+    './*.html',
+    './*.txt'
+  ];
+
+  return watch(filesToWatch, buildTarget);
 }
 
 function syncBrowser() {
   var files = [
-    './*.html',
-    './Recipes/**/*.html',
-    './css/*.css',
-    './images/*.{png,jpg,gif}',
-    './js/*.js'
+    './target/*.html',
+    './target/Recipes/**/*.html',
+    './target/css/*.css',
+    './target/images/*.{png,jpg,gif}',
+    './target/js/*.js'
   ];
 
   browserSync.init(files, {
     server: {
-      baseDir: "./dist"
+      baseDir: "./target"
     }
   });
 };
 
-// Default task
-const defaultTasks = parallel(syncBrowser, watchScss);
+function cleanTarget() {
+  return del(['target', 'gen']);
+}
 
+function copyNecessaryLocalNodeModules() {
+  return src(['./node_modules/bootstrap/**/*.*', './node_modules/jquery/**/*.*', './node_modules/popper.js/**/*.*'], {
+      base: './node_modules/'
+    })
+    .pipe(dest('./target/node_modules'));
+}
+
+function copyCssToTarget() {
+  return src('./scss/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(dest('./target/css'));
+}
+
+function copyImagesToTarget() {
+  return src('./images/*.{png,jpg,gif,svg}').pipe(dest('./target/images'));
+}
+
+function copyJavascriptToTarget() {
+  return src('./js/*.js').pipe(dest('./target/js'));
+}
+
+function copyRecipesToTarget() {
+  return src('./gen/Recipes/**/*.html').pipe(dest('./target/Recipes'));
+}
+
+function copyGuidesToTarget() {
+  return src('./Guides/**/*.html').pipe(dest('./target/Guides'));
+}
+
+function copyResourcesToTarget() {
+  return src('./Resources/**/*.html').pipe(dest('./target/Resources'));
+}
+
+function copyRootFilesToTarget() {
+  return src(['./*.html', './*.txt']).pipe(dest('./target'));
+}
 
 // Clean
 function clean() {
-  return del(['dist', 'gen']);
+  return del(['dist', 'gen', 'css']);
 }
-
 
 function copyFonts() {
   return src('./node_modules/font-awesome/fonts/**/*.{ttf,woff,eof,svg}*')
     .pipe(dest('./dist/fonts'));
+}
+
+function generateCss() {
+  return src('./scss/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(dest('./css'));
 }
 
 // // Images
@@ -134,7 +216,7 @@ function minifyRootHtml() {
 }
 
 function minifyRecipes() {
-    return src('./gen/Recipes/**/*.html')
+  return src('./gen/Recipes/**/*.html')
     .pipe(flatmap(function(stream, file) {
       return stream
         .pipe(usemin({
@@ -152,51 +234,50 @@ function minifyRecipes() {
     .pipe(dest('dist/Recipes/'));
 }
 
-// // Minify code
+
 function copyTextFilesToDist() {
   return src('./*.txt').pipe(dest('dist/'));
 };
 
-function getAllJsonFiles(dir, files) {
-  fs.readdirSync(dir).forEach(file => {
-    const current_file = path.join(dir, file);
-    if (fs.statSync(current_file).isDirectory())
-      return getAllJsonFiles(current_file, files);
-    else if (current_file.endsWith('.json'))
-      return files.push(current_file);
+function runDist() {
+  var files = [];
+
+  browserSync.init(files, {
+    server: {
+      baseDir: "./dist"
+    }
   });
-}
-
-function generateRecipesFromJson(callback) {
-  let template_as_string = fs.readFileSync('doT/recipe-template.jst', 'utf8');
-  let templateFunction = doT.template(template_as_string);
-
-  let files = [];
-  getAllJsonFiles("json/Recipes", files);
-  for (let index = 0; index < files.length; index++) {
-    let recipe_as_string = fs.readFileSync(files[index], 'utf8');
-    let recipe_as_json = JSON.parse(recipe_as_string);
-    let recipe_as_html = templateFunction(recipe_as_json);
-
-    let generated_recipe_file_path = "gen/" + files[index].substring(5).replace('.json', '.html');
-    fs_extra.ensureFileSync(generated_recipe_file_path);
-    fs.writeFileSync(generated_recipe_file_path, recipe_as_html);
-  }
-
-  callback();
 };
 
-// Build
-const build = series(clean, generateRecipesFromJson, copyFonts, parallel(minifyImages, minifyRecipes, minifyGuides, minifyResources, minifyRootHtml, copyTextFilesToDist));
+// TASKS
+
+const buildTarget = series(generateRecipesFromJson, 
+                            parallel( copyNecessaryLocalNodeModules, 
+                                      copyCssToTarget, 
+                                      copyImagesToTarget, 
+                                      copyJavascriptToTarget, 
+                                      copyRecipesToTarget, 
+                                      copyGuidesToTarget, 
+                                      copyResourcesToTarget, 
+                                      copyRootFilesToTarget));
+                            
+const build = series( clean, 
+                      generateRecipesFromJson, 
+                      generateCss, 
+                      copyFonts, 
+                      parallel( minifyImages, 
+                                minifyRecipes, 
+                                minifyGuides, 
+                                minifyResources, 
+                                minifyRootHtml, 
+                                copyTextFilesToDist));
+
+// Default task
+const runTarget = series( cleanTarget, 
+                          buildTarget,
+                          parallel( syncBrowser, 
+                                    watchAll));
 
 exports.build = build;
-exports.default = defaultTasks;
-exports.clean = clean;
-exports.copyFonts = copyFonts;
-exports.minifyImages = minifyImages;
-exports.minifyRecipes = minifyRecipes;
-exports.minifyGuides = minifyGuides;
-exports.minifyResources = minifyResources;
-exports.minifyRootHtml = minifyRootHtml;
-exports.copyTextFilesToDist = copyTextFilesToDist;
-exports.generateRecipesFromJson = generateRecipesFromJson;
+exports.runDist = runDist;
+exports.default = runTarget;
