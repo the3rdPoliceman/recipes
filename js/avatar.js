900a4d1d6fc847a3c55789c81018dfd5e4717277
the3rdPoliceman <gabbymoore@gmail.com>
2021-01-18 14:34:58 +0100

    added mouseover to avatar

---
+++ js/avatar.js
@@ -0,0 +1,7 @@
+function avatarOver(source) {
+    document.getElementById("avatar").src = "images/avatarDizzy.svg";
+}
+
+function avatarOut(source) {
+    document.getElementById("avatar").src = "images/avatar.svg";
+}
