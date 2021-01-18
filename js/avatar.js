bc44dcdd9c609b47cc15bbda16f0959846b340ef
the3rdPoliceman <gabbymoore@gmail.com>
2021-01-18 22:30:03 +0100

    added julia avatar

--- js/avatar.js
+++ js/avatar.js
@@ -1,7 +1,15 @@
 function avatarOver(source) {
-    document.getElementById("avatar").src = "images/avatarDizzy.svg";
+    document.getElementById("avatar").src = "/images/avatarDizzy.svg";
 }
 
 function avatarOut(source) {
-    document.getElementById("avatar").src = "images/avatar.svg";
+    document.getElementById("avatar").src = "/images/avatar.svg";
+}
+
+function avatarJuliaOver(source) {
+    document.getElementById("avatar").src = "/images/avatarJuliaDizzy.svg";
+}
+
+function avatarJuliaOut(source) {
+    document.getElementById("avatar").src = "/images/avatarJulia.svg";
 }
